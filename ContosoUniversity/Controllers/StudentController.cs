6b4b09b6297a8854e5fca31ba25fafab7fc2c1cf
umathan13@gmail.com <umathan13@gmail.com>
2015-08-10 15:57:35 -0400

    DetailsMethod-Enrollment
    added to foreach loop

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -54,12 +54,22 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
+            try
+            {
+
             if (ModelState.IsValid)
             {
                 db.Students.Add(student);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+           }
+        catch (DataException /* dex */)
+    {
+        //Log the error (uncomment dex variable name and add a line here to write a log.
+        ModelState.AddModelError("", "Unable to save changes.  Try again, if the problem persists to see your system administrator.");
+    }
 
             return View(student);
         }
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -12,5 +12,6 @@ namespace ContosoUniversity.Models
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+        
     }
 }
