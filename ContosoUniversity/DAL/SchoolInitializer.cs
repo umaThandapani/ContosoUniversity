28b7facd34ca507583e8e26cdd891f98668567e0
umathan13@gmail.com <umathan13@gmail.com>
2015-08-10 11:46:56 -0400

    Updated DAL SchoolInitializer
    Included Students and Courses

---
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                new Student{FirstMidName="Meredith", LastName="Alonso", EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Arturo", LastName="Anand", EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Gytis", LastName="Barzdukas", EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Yan", LastName="Li", EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Peggy", LastName="Justice", EnrollmentDate=DateTime.Parse("2001-09-01")},
+                new Student{FirstMidName="Laura", LastName="Norman", EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Nino", LastName="Olivetto", EnrollmentDate=DateTime.Parse("2005-09-01")}
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+            var courses = new List<Course>
+            {
+                new Course{CourseID=1050,Title="Chemistry", Credits=3,},
+                new Course{CourseID=4022,Title="Microeconomics", Credits=3,},
+                new Course{CourseID=4041,Title="Macroeconomics", Credits=3,},
+                new Course{CourseID=1045,Title="Calculus", Credits=4,},
+                new Course{CourseID=3141,Title="Trignometry", Credits=4,},
+                new Course{CourseID=2021,Title="Composition", Credits=3,},
+                new Course{CourseID=2042,Title="Literature", Credits=4,},
+               
+            };
+
+
+
+
+            base.Seed(context);
+        }
+
+
+    }
+}
