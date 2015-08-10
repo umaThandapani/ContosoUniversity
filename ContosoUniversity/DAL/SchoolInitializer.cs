b92fac4bcb6684a0e5acc7ba5ae5bdd157154a75
umathan13@gmail.com <umathan13@gmail.com>
2015-08-10 11:51:54 -0400

    Updated DAL
    Added data about course, grade, and students

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -36,13 +36,27 @@ namespace ContosoUniversity.DAL
                 new Course{CourseID=2042,Title="Literature", Credits=4,},
                
             };
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+            var enrollments = new List<Enrollment>
+            {
+                new Enrollment{StudentID=1, CourseID=1050, Grade=Grade.A},
+                new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+                new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+                new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+                new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+                new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+                new Enrollment{StudentID=3,CourseID=1050},
+                new Enrollment{StudentID=4,CourseID=1050,},
+                new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+                new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+                new Enrollment{StudentID=6,CourseID=1045},
+                new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
 
-
-
-
-            base.Seed(context);
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
         }
-
 
     }
 }
