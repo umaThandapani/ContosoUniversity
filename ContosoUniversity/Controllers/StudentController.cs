b67c797da3fd488d7c970b5e120a3dd32b688eb9
umathan13@gmail.com <umathan13@gmail.com>
2015-08-11 13:45:56 -0400

    Index ViewResult action
    Updated searchstring for students in SearchController

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -16,15 +16,59 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
          //GET: Student
-        public ActionResult Index()
+        //public ActionResult Index(string sortOrder)
+        //{
+        //    ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+        //    ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+        //    var students = from s in db.Students select s;
+
+        //    switch(sortOrder)
+        //    {
+        //        case "name_desc":
+        //            students = students.OrderByDescending(s => s.LastName);
+        //            break;
+        //        case "Date":
+        //            students = students.OrderBy(s => s.EnrollmentDate);
+        //            break;
+        //        case "date_desc":
+        //            students = students.OrderByDescending(s => s.EnrollmentDate);
+        //            break;
+        //        default:
+        //            students = students.OrderBy(s => s.LastName);
+        //            break;
+        //    }
+            
+        //    return View(students.ToList());
+        //}
+
+        public ViewResult Index(string sortOrder, string searchString)
         {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var students = from s in db.Students select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                    || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        students = students.OrderByDescending(s => s.LastName);
+                        break;
+                    case "Date":
+                        students = students.OrderBy(s => s.EnrollmentDate);
+                        break;
+                    case "date_desc":
+                        students = students.OrderByDescending(s => s.EnrollmentDate);
+                        break;
+                    default:
+                        students = students.OrderBy(s => s.LastName);
+                        break;
+                }
+            
             return View(db.Students.ToList());
         }
-
-        //public ViewResult Index()
-        //{
-        //    return View(db.Students.ToList());
-        //}
 
         // GET: Student/Details/5
         public ActionResult Details(int? id)
