e2fe9d49519d742560f75014fe8b29c1ff1d1916
umathan13@gmail.com <umathan13@gmail.com>
2015-08-12 12:07:48 -0400

    Throw Error

--- ContosoUniversity/Logging/ILogger.cs
+++ ContosoUniversity/Logging/ILogger.cs
@@ -12,10 +12,12 @@ namespace ContosoUniversity.Logging
         void Information(string message);
         void Information(string fmt, params object[] vars);
         void Information(Exception exception, string fmt, params object[] vars);
+        
         void Warning(string message);
         void Warning(string fmt, params object[] vars);
         void Warning(Exception exception, string fmt, params object[] vars);
 
+        
         void Error(string message);
         void Error(string fmt, params object[] vars);
         void Error(Exception exception, string fmt, params object[] vars);
