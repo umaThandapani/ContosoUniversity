3811ffb09ceb13fb1bb15af655740187b187cbe7
umathan13@gmail.com <umathan13@gmail.com>
2015-08-18 10:59:35 -0400

    Data Relations

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using ContosoUniversity.Models;
+﻿using ContosoUniversity.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-
-
 namespace ContosoUniversity.DAL
 {
-    public class SchoolContext : DbContext
-    {
-        public SchoolContext(): base("SchoolContext")
-        {
-
-        }
-
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Course> Courses { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
-    }
+public class SchoolContext : DbContext
+{
+public DbSet<Course> Courses { get; set; }
+public DbSet<Department> Departments { get; set; }
+public DbSet<Enrollment> Enrollments { get; set; }
+public DbSet<Instructor> Instructors { get; set; }
+public DbSet<Student> Students { get; set; }
+public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+protected override void OnModelCreating(DbModelBuilder modelBuilder)
+{
+modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+modelBuilder.Entity<Course>()
+    .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+.Map(t => t.MapLeftKey("CourseID")
+.MapRightKey("InstructorID")
+.ToTable("CourseInstructor"));
+}
+}
 }
