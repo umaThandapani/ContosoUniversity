e96a39b3f449f4cf5fff8db9ae421b15d1998c68
umathan13@gmail.com <umathan13@gmail.com>
2015-08-18 10:07:01 -0400

    Foreign Key

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -2,32 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
-    //class Enrollment
-    
-    //{
-    //}
-
-    public enum Grade
-    {
-        A, B, C, D, F
-    }
-
-    public class Enrollment
-    {
-        public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
-        public int StudentID { get; set; }
-        public Grade? Grade { get; set; }
-
-        public virtual Course Course { get; set; }
-        public virtual Student Student { get; set; }
-
-    }
-
-
+public enum Grade
+{
+A, B, C, D, F
+}
+public class Enrollment
+{
+public int EnrollmentID { get; set; }
+public int CourseID { get; set; }
+public int StudentID { get; set; } [DisplayFormat(NullDisplayText = "No grade")]
+public Grade? Grade { get; set; }
+public virtual Course Course { get; set; }
+public virtual Student Student { get; set; }
+}
 
 
 }
